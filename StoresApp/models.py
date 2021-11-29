58c118d505b148282ada782bba1c01d6903fc4aa
barbosa86 <barbosa86>
2021-11-29 16:02:22 +0000

    Import solved

--- StoresApp/models.py
+++ StoresApp/models.py
@@ -4,7 +4,7 @@ from django.db.models.fields import FloatField
 
 # Create your models here.
 
-class Store(models.Model):
+class Stores(models.Model):
     StoresId = models.AutoField(primary_key=True)
     StoresName = models.CharField(max_length=500)
     Latitude= models.FloatField()
